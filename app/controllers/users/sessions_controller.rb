3e4fbcb369c9955df77ef2dbc99df263e81b1b36
Prince Umunna <princevictor12@gmail.com>
2023-07-14 11:08:34 +0100

    customise devise sessions controller to respond_to :json

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,23 +1,33 @@
+# frozen_string_literal: true
+
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
   private
 
-  def respond_with(resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+  def respond_with(resource = current_user, _opts = {})
+    render json: {
+      status: { code: 200, message: 'Logged in sucessfully.' },
+      data: resource
+    }, status: :ok
   end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+                             Rails.application.credentials.fetch(:secret_key_base)).first
 
-    log_out_failure
-  end
+    current_user = User.find(jwt_payload['sub'])
 
-  def log_out_success
-    render json: { message: "You are logged out." }, status: :ok
-  end
-
-  def log_out_failure
-    render json: { message: "Hmm nothing happened."}, status: :unauthorized
+    if current_user
+      render json: {
+        status: 200,
+        message: 'logged out successfully'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 end
