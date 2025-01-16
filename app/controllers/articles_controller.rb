71ade5e127c127653a1dff0da6b74c9535b20035
Prince Umunna <princevictor12@gmail.com>
2025-01-16 13:58:07 +0000

    rubocop cleanup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,18 @@
-class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+# frozen_string_literal: true
 
+class ArticlesController < ApplicationController
   def index
-    @articles = Article.all 
-    render json: @articles 
+    @articles = Article.all
+    render json: @articles
   end
 
   def create
-    @article  = Artice.create(article_parmas)
+    @article = Artice.create(article_params)
+    render json: @article
   end
+  # Allow trusted Parameters
 
-  def article_parmas
-    params.require(:article). permit(:title, :body, :user_id)
+  def article_params
+    params.require(:article).permit(:title, :body, :user_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_133755) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_16_095424) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "jwt_denylist", force: :cascade do |t|
-    t.string "jti", null: false
-    t.datetime "exp", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_133755) do
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-end
 
+  add_foreign_key "articles", "users"
+end
